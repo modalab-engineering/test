9c18d8b5f28766b58e616e7b70787947f7c8e326
Alvin Gregory <alvingregory10k@gmail.com>
2025-07-15 17:14:20 -0400

    seems to be working. Needs error handling

--- infrastructure/dependencies/vector_db.py
+++ infrastructure/dependencies/vector_db.py
@@ -39,19 +39,21 @@ class VertexVectorDBClient:
     def search(
         self, query_vector: list[float], limit: int, followed_stores: List[int] = None
     ):
-        datapoint = aiplatform_v1.IndexDatapoint(feature_vector=query_vector)
+        datapoint_kwargs = {"feature_vector": query_vector}
 
-        query_filter = None
         if followed_stores:
             string_followed_stores = [str(store_id) for store_id in followed_stores]
-            query_filter = aiplatform_v1.IndexDatapoint.Restriction(
-                namespace="store_id", allow_list=string_followed_stores
-            )
+            datapoint_kwargs["restricts"] = [
+                aiplatform_v1.IndexDatapoint.Restriction(
+                    namespace="store_id", allow_list=string_followed_stores
+                )
+            ]
+
+        datapoint = aiplatform_v1.IndexDatapoint(**datapoint_kwargs)
 
         query = aiplatform_v1.FindNeighborsRequest.Query(
             datapoint=datapoint,
             neighbor_count=limit,
-            restricts=[query_filter] if query_filter else [],
         )
 
         request = aiplatform_v1.FindNeighborsRequest(
